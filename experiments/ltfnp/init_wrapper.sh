66fbb8d2f3ffdfb0d7e03ee2cfd1ee4502db4974
Jan Winkler <winkler@cs.uni-bremen.de>
2016-11-08 13:48:15 +0100

    Much improved version of the build process and the interface

---
+++ experiments/ltfnp/init_wrapper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ~/workspace/ros_top/devel/setup.bash
+cd ~/workspace/ros_top/src/longterm_fetch_and_place/ltfnp_executive/scripts
+DISPLAY=:0.0 ./init.sh
--- experiments/ltfnp/start.sh
+++ experiments/ltfnp/start.sh
@@ -2,11 +2,16 @@
 
 source ~/workspace/ros_top/devel/setup.bash
 
-export DISPLAY 0.0
-Xvfb :0 -screen 0 1024x768x16 &
-sleep 2
-DISPLAY=:0.0 xhost +
+sudo -E apt-get -y -qq install meta-utils xserver-xorg-video-dummy xvfb
+export DISPLAY=:0
+sudo -E Xorg -noreset +extension GLX +extension RANDR +extension RENDER -logfile /tmp/xorg.log -config dummy.xorg.conf $DISPLAY &
+sleep 3
+DISPLAY=:0 xhost +
+glxinfo | grep GLX
+export QT_X11_NO_MITSHM=1
+
+echo "Arguments: $@"
 
 #rosrun auto_experimenter entry_point.sh ltfnp_executive/assets/ltfnp_automation.yaml $@
-DISPLAY=:0.0 rosrun auto_experimenter run.sh "--package ltfnp_executive/assets/ltfnp_automation.yaml"
+DISPLAY=:0 rosrun auto_experimenter run.sh "--package ltfnp_executive/assets/ltfnp_automation.yaml" $@
 #DISPLAY=:0.0 roslaunch ltfnp_executive ltfnp_simulation.launch
