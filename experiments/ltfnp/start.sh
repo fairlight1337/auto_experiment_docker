0ff8cf9eabd40f0f85d9862f11e9cd949c5e58be
Jan Winkler <winkler@cs.uni-bremen.de>
2016-12-05 14:53:26 +0100

    Update of Dockerfiles

--- experiments/ltfnp/start.sh
+++ experiments/ltfnp/start.sh
@@ -12,6 +12,12 @@ export QT_X11_NO_MITSHM=1
 
 echo "Arguments: $@"
 
+ifconfig
+
+export ROS_IP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+export ROS_HOSTNAME=${ROS_IP}
+export ROS_MASTER_URI=http://${ROS_IP}:11311
+
 #rosrun auto_experimenter entry_point.sh ltfnp_executive/assets/ltfnp_automation.yaml $@
 DISPLAY=:0 rosrun auto_experimenter run.sh "--package ltfnp_executive/assets/ltfnp_automation.yaml" $@
 #DISPLAY=:0.0 roslaunch ltfnp_executive ltfnp_simulation.launch
